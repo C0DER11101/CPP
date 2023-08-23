55dee920afff6312b1bae5a0f8872285fdf6e7b7
Priyanuj Bora <while.alive.program@gmail.com>
2023-08-23 16:31:13 +0530

    Constructors and Destructors in C++

---
+++ ConstructorsDestructors/Programs/parameterizedCons.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class sample{
+	int m, n;
+
+	public:
+	sample(int x, int y);
+	void show();
+};
+
+sample::sample(int x, int y){
+	m=x, n=y;
+}
+
+void sample::show(){
+	cout<<"m = "<<m<<"\n";
+	cout<<"n = "<<n<<"\n";
+}
+
+int main(void){
+	sample smp=sample(100, 200); // explicit constructor call
+
+	smp.show();
+
+	return 0;
+}
