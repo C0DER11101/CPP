8173608b1bac7da1bc6f0ef5177fca24efaa0db1
Priyanuj Bora <while.alive.program@gmail.com>
2023-09-02 21:44:58 +0530

    Ambiguity resolution-2

---
+++ Inheritance/Programs/AmbiguityRes2.cpp
@@ -0,0 +1,49 @@
+// Resolution-1 to ambiguity
+
+
+#include<iostream>
+
+using namespace std;
+
+class Super1{
+	int sup1Val;
+
+	public:
+	void display(){
+		sup1Val=100;
+		cout<<"sup1Val = "<<sup1Val<<"\n";
+	}
+};
+
+class Super2{
+	int sup2Val;
+
+	public:
+	void display(){
+		sup2Val=120;
+		cout<<"sup2Val = "<<sup2Val<<"\n";
+	}
+};
+
+class Sub:public Super1, public Super2{
+	int subVal;
+
+	public:
+	void display(){
+		subVal=140;
+		cout<<"subVal = "<<subVal<<"\n";
+	}
+};
+
+
+int main(void){
+	Sub sub;
+
+	sub.Super1::display();       // calls display() of the class Super1!!
+
+	sub.Super2::display();       // calls display() of the class Super2!!
+
+	sub.display();
+
+	return 0;
+}
