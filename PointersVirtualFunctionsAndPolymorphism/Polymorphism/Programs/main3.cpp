a2970ffc2201018427fdb3c4c029b1d977d38724
Priyanuj Bora <while.alive.program@gmail.com>
2023-09-07 06:47:49 +0530

    Problem with base class pointer

---
+++ PointersVirtualFunctionsAndPolymorphism/Polymorphism/Programs/main3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+class Super{
+
+	public:
+		void display(){
+			cout<<"In Super\n";
+		}
+};
+
+class Sub: public Super{
+
+	public:
+		void display(){
+			cout<<"In Super\n";
+		}
+};
+
+int main(void){
+
+	Super* super;
+
+	Super base;
+	Sub sub;
+
+	super=&base;
+
+	super->display();
+
+	super=&sub;
+
+	super->display();                // STILL CALLS display() OF CLASS Super!
+
+	return 0;
+}
