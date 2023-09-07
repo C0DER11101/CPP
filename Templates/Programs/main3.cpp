f350299dcb99cae65d20f93ec284c64a2166545b
Priyanuj Bora <while.alive.program@gmail.com>
2023-09-07 18:35:54 +0530

    Default types in templates

---
+++ Templates/Programs/main3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename T1=int, typename T2=char>
+class Super{
+	T1 a;
+	T2 b;
+
+	public:
+	Super(T1 val1, T2 val2):
+		a(val1), b(val2){
+			cout<<"Super object created!\n";
+		}
+
+	void show(){
+		cout<<"a = "<<a<<" & b = "<<b<<"\n";
+	}
+};
+
+int main(void){
+
+	Super<> superObj1(12, 'a');
+
+	Super<char, double> superObj2('b', 34.5);
+
+	cout<<"Super object-1\n";
+	superObj1.show();
+
+	cout<<"Super object-2\n";
+	superObj2.show();
+
+	return 0;
+}
